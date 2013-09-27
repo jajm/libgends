32a71e4271a9c64e083dd21da69f6383ce2e7977
Julian Maurice <jajm@jajm.org>
2013-09-27 08:57:41 +0200

    Add hash_map_fast data structure

---
+++ include/hash_map_fast.h
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2013 Julian Maurice
+ *
+ * This file is part of libgends
+ *
+ * libgends is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libgends is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libgends.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * File              : hash_map_fast.h
+ * Short description : hash map management
+ */
+
+#ifndef gds_hash_map_fast_h_included
+#define gds_hash_map_fast_h_included
+
+#include <stdint.h>
+#include "callbacks.h"
+#include "rbtree.h"
+
+struct gds_hash_map_fast_s {
+	uint32_t size;
+	gds_rbtree_node_t **map;
+	gds_hash_cb hash_cb;
+	gds_cmpkey_cb cmpkey_cb;
+};
+typedef struct gds_hash_map_fast_s gds_hash_map_fast_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Create a new hash map */
+/* size: the number of buckets
+ *   hash_cb : hash callback
+ * cmpkey_cb : cmpkey callback */
+/* Return: a pointer to the new hash map */
+gds_hash_map_fast_t *
+gds_hash_map_fast_new(
+	uint32_t size,
+	gds_hash_cb hash_cb,
+	gds_cmpkey_cb cmpkey_cb
+);
+
+/* Set a key/value pair in the hash map */
+/* If key already exists, old value is replaced by the new one */
+/*            h : pointer to the hash map
+ *          key : key to insert
+ *         data : data to insert
+ *      free_cb : free callback for data */
+/* Return: 0: key was already in the hash map
+ *         1: key was just added */
+int
+gds_hash_map_fast_set(
+	gds_hash_map_fast_t *h,
+	void *key,
+	void *data,
+	gds_free_cb free_cb
+);
+
+/* Get a value given its key */
+/*         h : pointer to the hash map
+ *       key : key to unset */
+/* Return: pointer to the data, or NULL if key doesn't exist */
+void *
+gds_hash_map_fast_get(
+	gds_hash_map_fast_t *h,
+	const void *key
+);
+
+/* Unset a key/value pair in the hash map */
+/*           h : pointer to the hash map
+ *         key : key to unset
+ * key_free_cb : free callback for key
+ *     free_cb : free callback for data */
+/* Return: 0: key was correctly unset
+ *         1: key was not in the hash map */
+int
+gds_hash_map_fast_unset(
+	gds_hash_map_fast_t *h,
+	const void *key,
+	gds_free_cb key_free_cb,
+	gds_free_cb free_cb
+);
+
+gds_iterator_t *
+gds_hash_map_fast_iterator_new(
+	gds_hash_map_fast_t *h
+);
+
+#define gds_hash_map_fast_foreach(key, val, hash) \
+	for (gds_iterator_t *gds_hash_map_fast_it = gds_hash_map_fast_iterator_new(hash) \
+		; gds_hash_map_fast_it != NULL \
+		; gds_iterator_free(gds_hash_map_fast_it), gds_hash_map_fast_it = NULL) \
+	while (!gds_iterator_step(gds_hash_map_fast_it) \
+		&& ((key = gds_iterator_getkey(gds_hash_map_fast_it)) || !key) \
+		&& ((val = gds_iterator_get(gds_hash_map_fast_it)) || !val))
+
+/* Return keys contained in the hash map as a list */
+/* h : pointer to the hash map */
+/* Return: pointer to the list */
+gds_slist_t *
+gds_hash_map_fast_keys(
+	gds_hash_map_fast_t *h
+);
+
+/* Return values contained in the hash map as a list */
+/* h : pointer to the hash map */
+/* Return: pointer to the list */
+gds_slist_t *
+gds_hash_map_fast_values(
+	gds_hash_map_fast_t *h
+);
+
+/* Change the number of buckets of hash map */
+/*        h : pointer to the hash map
+ * new_size : New size (number of buckets) */
+void
+gds_hash_map_fast_change_size(
+	gds_hash_map_fast_t *h,
+	uint32_t new_size
+);
+
+/* Free hash map */
+/*           h : pointer to the hash map
+ * key_free_cb : free callback for key
+ *     free_cb : free callback for data */
+void
+gds_hash_map_fast_free(
+	gds_hash_map_fast_t *h,
+	gds_free_cb key_free_cb,
+	gds_free_cb free_cb
+);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* Not gds_hash_map_fast_h_included */
+
---
+++ src/hash_map_fast.c
@@ -0,0 +1,280 @@
+/*
+ * Copyright 2012-2013 Julian Maurice
+ *
+ * This file is part of libgends
+ *
+ * libgends is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libgends is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libgends.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include "slist.h"
+#include "exception.h"
+#include "check_arg.h"
+#include "log.h"
+#include "rbtree.h"
+#include "hash_map_fast.h"
+
+gds_hash_map_fast_t * gds_hash_map_fast_new(uint32_t size, gds_hash_cb hash_cb,
+	gds_cmpkey_cb cmpkey_cb)
+{
+	gds_hash_map_fast_t *h;
+
+	GDS_CHECK_ARG_NOT_ZERO(size);
+	GDS_CHECK_ARG_NOT_NULL(hash_cb)
+	GDS_CHECK_ARG_NOT_NULL(cmpkey_cb)
+
+	h = malloc(sizeof(gds_hash_map_fast_t));
+	if (h == NULL) {
+		GDS_THROW_ALLOC_ERROR(sizeof(gds_hash_map_fast_t));
+	}
+
+	h->map = calloc(size, sizeof(gds_rbtree_node_t *));
+	if (h->map == NULL) {
+		GDS_THROW_ALLOC_ERROR(sizeof(gds_rbtree_node_t *));
+	}
+
+	h->size = size;
+	h->hash_cb = hash_cb;
+	h->cmpkey_cb = cmpkey_cb;
+
+	return h;
+}
+
+uint32_t gds_hash_map_fast_hash(gds_hash_map_fast_t *h, const void *key)
+{
+	return h->hash_cb(key, h->size) % h->size;
+}
+
+int gds_hash_map_fast_set(gds_hash_map_fast_t *h, void *key, void *data,
+	gds_free_cb free_cb)
+{
+	uint32_t hash;
+
+	GDS_CHECK_ARG_NOT_NULL(h);
+
+	hash = gds_hash_map_fast_hash(h, key);
+	return gds_rbtree_set(&(h->map[hash]), key, data, h->cmpkey_cb,
+		free_cb);
+}
+
+void * gds_hash_map_fast_get(gds_hash_map_fast_t *h, const void *key)
+{
+	uint32_t hash;
+	void *data;
+
+	GDS_CHECK_ARG_NOT_NULL(h);
+
+	hash = gds_hash_map_fast_hash(h, key);
+	data = gds_rbtree_get(h->map[hash], key, h->cmpkey_cb);
+	return data;
+}
+
+int gds_hash_map_fast_unset(gds_hash_map_fast_t *h, const void *key,
+	gds_free_cb key_free_cb, gds_free_cb free_cb)
+{
+	uint32_t hash;
+	int rv;
+
+	GDS_CHECK_ARG_NOT_NULL(h);
+
+	hash = gds_hash_map_fast_hash(h, key);
+	rv = gds_rbtree_del(&(h->map[hash]), key, h->cmpkey_cb,
+		key_free_cb, free_cb);
+
+	return rv;
+}
+
+typedef struct {
+	gds_hash_map_fast_t *hash;
+	uint32_t i;
+	gds_iterator_t *rbtree_it;
+} gds_hash_map_fast_iterator_data_t;
+
+void gds_hash_map_fast_iterator_find_next_rbtree(gds_hash_map_fast_iterator_data_t *data)
+{
+	gds_hash_map_fast_t *hash = data->hash;
+	uint32_t i = data->i;
+
+	while (i < hash->size && hash->map[i] == NULL)
+		i++;
+
+	gds_iterator_free(data->rbtree_it);
+	data->rbtree_it = NULL;
+
+	if (i < hash->size) {
+		data->rbtree_it = gds_rbtree_iterator_new(hash->map[i]);
+	}
+
+	data->i = i;
+}
+
+int gds_hash_map_fast_iterator_reset(gds_hash_map_fast_iterator_data_t *data)
+{
+	data->i = 0;
+	gds_hash_map_fast_iterator_find_next_rbtree(data);
+
+	return 0;
+}
+
+int gds_hash_map_fast_iterator_step(gds_hash_map_fast_iterator_data_t *data)
+{
+	int rc;
+
+	if (data->rbtree_it == NULL)
+		return 1;
+
+	rc = gds_iterator_step(data->rbtree_it);
+	if (rc > 0) {
+		data->i++;
+		gds_hash_map_fast_iterator_find_next_rbtree(data);
+		if (data->rbtree_it != NULL) {
+			rc = gds_iterator_step(data->rbtree_it);
+		}
+	}
+
+	return rc;
+}
+
+void * gds_hash_map_fast_iterator_get(gds_hash_map_fast_iterator_data_t *data)
+{
+	return gds_iterator_get(data->rbtree_it);
+}
+
+void * gds_hash_map_fast_iterator_getkey(gds_hash_map_fast_iterator_data_t *data)
+{
+	return gds_iterator_getkey(data->rbtree_it);
+}
+
+void gds_hash_map_fast_iterator_data_free(gds_hash_map_fast_iterator_data_t *data)
+{
+	gds_iterator_free(data->rbtree_it);
+	free(data);
+}
+
+gds_iterator_t * gds_hash_map_fast_iterator_new(gds_hash_map_fast_t *h)
+{
+	gds_hash_map_fast_iterator_data_t *data;
+	gds_iterator_t *it;
+
+	data = malloc(sizeof(gds_hash_map_fast_iterator_data_t));
+	if (data == NULL) {
+		GDS_THROW_ALLOC_ERROR(sizeof(gds_hash_map_fast_iterator_data_t));
+	}
+
+	data->hash = h;
+	data->rbtree_it = NULL;
+
+	it = gds_iterator_new(data,
+		(gds_iterator_reset_cb) gds_hash_map_fast_iterator_reset,
+		(gds_iterator_step_cb) gds_hash_map_fast_iterator_step,
+		(gds_iterator_get_cb) gds_hash_map_fast_iterator_get,
+		(gds_iterator_getkey_cb) gds_hash_map_fast_iterator_getkey,
+		(gds_free_cb) gds_hash_map_fast_iterator_data_free);
+
+	return it;
+}
+
+gds_slist_t * gds_hash_map_fast_keys(gds_hash_map_fast_t *h)
+{
+	gds_slist_t *l = gds_slist_new();
+	gds_slist_t *list;
+	uint32_t i;
+
+	GDS_CHECK_ARG_NOT_NULL(h);
+
+	for (i = h->size; i > 0; i--) {
+		list = gds_rbtree_keys(h->map[i-1]);
+		if (list != NULL) {
+			gds_slist_splice(l, 0, 0, NULL, NULL, list);
+			gds_slist_free(list, NULL, NULL);
+		}
+	}
+
+	return l;
+}
+
+gds_slist_t * gds_hash_map_fast_values(gds_hash_map_fast_t *h)
+{
+	gds_slist_t *l = gds_slist_new();
+	gds_slist_t *list;
+	uint32_t i;
+
+	GDS_CHECK_ARG_NOT_NULL(h);
+
+	for (i = h->size; i > 0; i--) {
+		list = gds_rbtree_values(h->map[i-1]);
+		if (list != NULL) {
+			gds_slist_splice(l, 0, 0, NULL, NULL, list);
+			gds_slist_free(list, NULL, NULL);
+		}
+	}
+
+	return l;
+}
+
+gds_rbtree_node_t ** gds_hash_map_fast_build_map(gds_hash_map_fast_t *h, uint32_t size)
+{
+	gds_rbtree_node_t **map;
+	gds_iterator_t *it;
+	uint32_t hash;
+	void *k, *v;
+
+	map = calloc(size, sizeof(gds_rbtree_node_t *));
+	if (map == NULL) {
+		GDS_THROW_ALLOC_ERROR(sizeof(gds_rbtree_node_t *));
+	}
+	it = gds_hash_map_fast_iterator_new(h);
+	gds_iterator_reset(it);
+	while (!gds_iterator_step(it)) {
+		v = gds_iterator_get(it);
+		k = gds_iterator_getkey(it);
+		hash = h->hash_cb(k, size) % size;
+		gds_rbtree_add(&(map[hash]), k, v, h->cmpkey_cb);
+	}
+	gds_iterator_free(it);
+
+	return map;
+}
+
+void gds_hash_map_fast_change_size(gds_hash_map_fast_t *h, uint32_t new_size)
+{
+	gds_rbtree_node_t **map;
+
+	GDS_CHECK_ARG_NOT_NULL(h);
+	GDS_CHECK_ARG_NOT_ZERO(new_size);
+
+	map = gds_hash_map_fast_build_map(h, new_size);
+	for (uint32_t i = 0; i < h->size; i++) {
+		gds_rbtree_free(h->map[i], NULL, NULL);
+	}
+	free(h->map);
+	h->map = map;
+	h->size = new_size;
+}
+
+void gds_hash_map_fast_free(gds_hash_map_fast_t *h, gds_free_cb key_free_cb,
+	gds_free_cb free_cb)
+{
+	uint32_t i;
+	if (h != NULL) {
+		for (i=0; i<h->size; i++) {
+			gds_rbtree_free(h->map[i], key_free_cb,
+				free_cb);
+		}
+		free(h->map);
+		free(h);
+	}
+}
