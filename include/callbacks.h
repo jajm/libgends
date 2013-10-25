43b828a9fe76575ae8d9c9f9144d2c2012a40b8f
Julian Maurice <jajm@jajm.org>
2013-10-26 00:27:30 +0200

    Reformat comments in callbacks.h

--- include/callbacks.h
+++ include/callbacks.h
@@ -22,43 +22,58 @@
 
 #include <stdint.h>
 
-/* Callback: gds_free_cb */
-/* Purpose: Free memory used by an object */
-/* 1st parameter: pointer to object */
-/* Should returns: nothing */
-typedef void (*gds_free_cb)(void *);
+/* Free memory used by an object
+ *
+ * Parameters
+ *   ptr : pointer to object
+ */
+typedef void (*gds_free_cb)(void *ptr);
 
-/* Callback: gds_getkey_cb */
-/* Purpose: Retrieve the key of an object */
-/* 1st parameter: pointer to object */
-/* Should returns: key of object */
-typedef void * (*gds_getkey_cb)(const void *);
+/* Retrieve the key of an object
+ *
+ * Parameters
+ *   ptr : pointer to object
+ *
+ * Should returns
+ *   key of object
+ */
+typedef void * (*gds_getkey_cb)(const void *ptr);
 
-/* Callback: gds_cmpkey_cb */
-/* Purpose: Compare two keys */
-/* 1st paramater: a key as returned by gds_getkey_cb (key1)
- * 2nd paramater: a key as returned by gds_getkey_cb (key2) */
-/* Should returns:
- *  - if key1 < key2: a negative value
- *  - if key1 = key2: 0 (zero)
- *  - if key1 > key2: a positive value */
-typedef int (*gds_cmpkey_cb)(const void *, const void *);
+/* Compare two keys
+ *
+ * Parameters
+ *   key1 : a key as returned by gds_getkey_cb
+ *   key2 : a key as returned by gds_getkey_cb
+ *
+ * Should returns
+ *   if key1 < key2: a negative value
+ *   if key1 = key2: 0 (zero)
+ *   if key1 > key2: a positive value
+ */
+typedef int (*gds_cmpkey_cb)(const void *key1, const void *key2);
 
-/* Callback: gds_cmp_cb */
-/* Purpose: Compare two objects */
-/* 1st parameter: first object to compare (object1)
- * 2nd parameter: second object to compare (object2) */
-/* Should returns:
- *  - if object1 < object2: a negative value
- *  - if object1 = object2: 0 (zero)
- *  - if object1 > object2: a positive value */
-typedef int (*gds_cmp_cb)(const void *, const void *);
+/* Compare two objects
+ *
+ * Parameters
+ *   ptr1 : first object to compare
+ *   ptr2 : second object to compare
+ *
+ * Should returns
+ *   if object1 < object2: a negative value
+ *   if object1 = object2: 0 (zero)
+ *   if object1 > object2: a positive value
+ */
+typedef int (*gds_cmp_cb)(const void *ptr1, const void *ptr2);
 
-/* Callback: gds_hash_cb */
-/* Purpose: Compute the hash of an object */
-/* 1st parameter: object to compute
- * 2nd parameter: size of hash map */
-/* Should returns: hash (integer) of the object */
-typedef uint32_t (*gds_hash_cb)(const void *, uint32_t);
+/* Compute the hash of an object
+ *
+ * Parameters
+ *   ptr  : object to compute
+ *   size : size of hash map
+ *
+ * Should returns:
+ *   hash (integer) of the object
+ */
+typedef uint32_t (*gds_hash_cb)(const void *ptr, uint32_t size);
 
 #endif /* Not callbacks_h_included */
